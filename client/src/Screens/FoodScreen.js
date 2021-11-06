8b5994edca9cac24428c9dbf30c5777ea9a602f0
adriangeorge <cgeorgeadrian99@gmail.com>
2021-11-06 22:13:38 +0200

    Add login screen

--- client/src/Screens/FoodScreen.js
+++ client/src/Screens/FoodScreen.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import FoodCard from '../Components/FoodCard';
 
 const FoodScreen = () => {
-	return <div className="flex-grow">food</div>;
+    const Colors = ['orange', 'yellow', 'lightGreen', 'darkGreen'];
+
+    Array.prototype.random = function () {
+        return this[Math.floor(Math.random() * this.length)];
+    };
+
+    return (
+        <div className="flex-grow px-3 py-4">
+            <FoodCard
+                title="Adderall"
+                description="Tragi pe nas"
+                points={15}
+                color={Colors.random()}
+                type={1}
+            />
+            <FoodCard
+                title="Adderall"
+                description="Tragi pe nas"
+                points={10}
+                color={Colors.random()}
+                type={1}
+            />
+            <FoodCard
+                title="Adderall"
+                description="Tragi pe nas"
+                points={20}
+                color={Colors.random()}
+                type={1}
+            />
+            <FoodCard
+                title="Savarina"
+                description="Desert"
+                points={15}
+                color={Colors.random()}
+                type={-1}
+            />
+
+        </div>
+
+    );
 };
 
 export default FoodScreen;
--- client/src/Screens/RegisterScreen.js
+++ client/src/Screens/RegisterScreen.js
@@ -1,89 +1,89 @@
 import React, { useState } from 'react';
 
 const RegisterScreen = () => {
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [confirmPassword, setConfirmPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-	return (
-		<div className="flex flex-col gap-3 flex-grow justify-center items-center">
-			<div>
-				<div className="flex flex-col py-3 gap-1">
-					<label htmlFor="" className="text-white text-xl">
-						First Name
-					</label>
-					<input
-						onChange={(e) => setFirstName(e.target.value)}
-						type="text"
-						className="text-white bg-transparent border-b"
-					/>
-				</div>
+    return (
+        <div className="flex flex-col gap-3 flex-grow justify-center items-center">
+            <div>
+                <div className="flex flex-col py-3 gap-1">
+                    <label htmlFor="" className="text-white text-xl">
+                        First Name
+                    </label>
+                    <input
+                        onChange={(e) => setFirstName(e.target.value)}
+                        type="text"
+                        className="text-white bg-transparent border-b"
+                    />
+                </div>
 
-				<div className="flex flex-col py-3 gap-1">
-					<label htmlFor="" className="text-white text-xl">
-						Last Name
-					</label>
-					<input
-						onChange={(e) => setLastName(e.target.value)}
-						type="text"
-						className="text-white bg-transparent border-b"
-					/>
-				</div>
+                <div className="flex flex-col py-3 gap-1">
+                    <label htmlFor="" className="text-white text-xl">
+                        Last Name
+                    </label>
+                    <input
+                        onChange={(e) => setLastName(e.target.value)}
+                        type="text"
+                        className="text-white bg-transparent border-b"
+                    />
+                </div>
 
-				<div className="flex flex-col py-3 gap-1">
-					<label htmlFor="" className="text-white text-xl">
-						Email
-					</label>
-					<input
-						onChange={(e) => setEmail(e.target.value)}
-						type="text"
-						className="text-white bg-transparent border-b"
-					/>
-				</div>
+                <div className="flex flex-col py-3 gap-1">
+                    <label htmlFor="" className="text-white text-xl">
+                        Email
+                    </label>
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="text"
+                        className="text-white bg-transparent border-b"
+                    />
+                </div>
 
-				<div className="flex flex-col py-3 gap-1">
-					<label htmlFor="" className="text-white text-xl">
-						Password
-					</label>
-					<input
-						onChange={(e) => setPassword(e.target.value)}
-						type="password"
-						className="text-white bg-transparent border-b"
-					/>
-				</div>
+                <div className="flex flex-col py-3 gap-1">
+                    <label htmlFor="" className="text-white text-xl">
+                        Password
+                    </label>
+                    <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        className="text-white bg-transparent border-b"
+                    />
+                </div>
 
-				<div className="flex flex-col py-3 gap-1">
-					<label htmlFor="" className="text-white text-xl">
-						Confirm Password
-					</label>
-					<input
-						onChange={(e) => setConfirmPassword(e.target.value)}
-						type="password"
-						className="text-white bg-transparent border-b"
-					/>
-				</div>
+                <div className="flex flex-col py-3 gap-1">
+                    <label htmlFor="" className="text-white text-xl">
+                        Confirm Password
+                    </label>
+                    <input
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        type="password"
+                        className="text-white bg-transparent border-b"
+                    />
+                </div>
 
-				<div className="flex py-3 justify-between">
-					<label className="text-xl text-white" htmlFor="">
-						Role
-					</label>
-					<select
-						className="text-white text-lg bg-transparent"
-						name="role"
-						id=""
-					>
-						<option value="parent">Parent</option>
-						<option value="child">Child</option>
-					</select>
-				</div>
-			</div>
-			<div className="bg-darkGreen p-3 text-white text-xl border rounded-xl">
-				Submit
-			</div>
-		</div>
-	);
+                <div className="flex py-3 justify-between">
+                    <label className="text-xl text-white" htmlFor="">
+                        Role
+                    </label>
+                    <select
+                        className="text-white text-lg bg-transparent"
+                        name="role"
+                        id=""
+                    >
+                        <option value="parent">Parent</option>
+                        <option value="child">Child</option>
+                    </select>
+                </div>
+            </div>
+            <div className="bg-darkGreen p-3 text-white text-xl border rounded-xl">
+                Submit
+            </div>
+        </div>
+    );
 };
 
 export default RegisterScreen;
